991228c7a8dff9ec62060f74be076dc331dd1b65
charraig <meredith.miller10@gmail.com>
2020-05-26 17:50:59 -0400

    removed cruft

--- eda/predictors.R
+++
@@ -1,8 +0,0 @@
-library(DataExplorer)
-library(ggplot2)
-
-# Generic profiling report
-create_report(info_df, 
-              output_file = 'predictors_report.html',
-              output_dir = './eda')
-
